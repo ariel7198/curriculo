4a87f7fa3da90c5d1354f18f8c71f5e6bc81f35c
ariel7198 <ariel.7198@hotmail.com>
2023-06-18 15:03:41 -0300

    atualizacao de experiencias

--- ariel-campos/src/components/pages/Experience.jsx
+++ ariel-campos/src/components/pages/Experience.jsx
@@ -7,12 +7,27 @@ import Companycard from '../Companycard'
 import compumateLogo from '../../images/compumate.png'
 import fbnetLogo from '../../images/fbnet.png'
 import digimaxLogo from '../../images/digimax.png'
+import ndlogo from '../../images/nd.png'
 
 const Experience = () => {
     return (
         <div className={style.experienciaContainer}>
             <h2> Experiência </h2>
             <div className={style.companyCardsContainer}>
+                 <Companycard
+                    empresa='Nação Digital'
+                    periodo='2021 - 2023'
+                    imagem={ndlogo}
+                    cargo='Desenvolvedor Web'
+                    descricao={
+                        <ul>
+                            <li> Responsável pela criação e manutenção de sites em diferentes plataformas </li>
+                            <li> Responsável pelo desenvolvimento e implementação de sistemas para soluções internas da empresa;</li>
+                            <li> Responsável por coordenar o time de desenvolvimento nos projetos internos seguindo metodologias ágeis; </li>
+                            <li> Responsável pela relação entre o time de desenvolvimento e outras áreas da empresa envolvidas nos projetos.</li>
+                        </ul>
+                    }
+                />
                 <Companycard
                     empresa='Digimax'
                     periodo='2019 - Atual'
@@ -27,7 +42,7 @@ const Experience = () => {
                         </ul>
                     }
                 />
-                <Companycard
+                {/* <Companycard
                     empresa='Compumate'
                     periodo='2019 - 2019'
                     imagem={compumateLogo}
@@ -43,8 +58,8 @@ const Experience = () => {
                             <li> Instalação e manutenção de terminais e servidores. </li>
                         </ul>
                     }
-                />
-                <Companycard
+                /> */}
+                {/* <Companycard
                     empresa='FbNet'
                     periodo='2018 - 2019'
                     imagem={fbnetLogo}
@@ -56,7 +71,7 @@ const Experience = () => {
                             <li> Configuração de roteadores openwrt e dispositivos mikrotik. </li>
                         </ul>
                     }
-                />
+                /> */}
 
             </div>
         </div>
